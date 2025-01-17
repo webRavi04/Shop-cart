5d2892739fcdb743cf4f8fdbfd8d95f6e2642d73
Ravi <ravimewada97@gmail.com>
2025-01-17 19:42:59 +0530

    product api

--- backend/controllers/productController.ts
+++ backend/controllers/productController.ts
@@ -23,3 +23,53 @@ export const getAllProducts = async (req: Request, res: Response) => {
         res.status(500).json({ message: 'Error retrieving products', error });
     }
 };
+
+export const getProductById = async (req: Request, res: Response) => {
+    try {
+        console.log("test", req.params);
+        const { id } = req.params;
+        const product = await db.Product.findByPk(id);
+        console.log(product)
+        if (!product) {
+            res.status(404).json({ message: 'Product not found' });
+        }
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: 'Error retrieving product', error });
+    }
+}
+
+export const updateProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { name, category, price } = req.body;
+        const product = await db.Product.findByPk(id);
+        if (!product) {
+            res.status(404).json({ message: 'Product not found' });
+        }
+
+        product.name = name || product.name;
+        product.category = category || product.category;
+        product.price = price || product.price;
+
+        await product.save();
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: 'Error in update product', error });
+    }
+}
+
+export const deleteProduct = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const product = await db.Product.findByPk(id);
+        console.log(product);
+        if (!product) {
+            res.status(404).json({ message: 'Product not found' });
+        }
+        await product.destroy();
+        res.status(200).json({ message: 'Product successfully deleted' });
+    } catch (error) {
+        res.status(500).json({ message: 'Error in delete product', error });
+    }
+}
--- backend/routes/product.route.ts
+++ backend/routes/product.route.ts
@@ -1,15 +1,18 @@
 import { Router } from 'express';
 import {
     createProduct,
-    getAllProducts
+    deleteProduct,
+    getAllProducts,
+    getProductById,
+    updateProduct
 } from '../controllers/productController';
 
 const router = Router();
 
 router.post('/products', createProduct);
 router.get('/products', getAllProducts);
-// router.get('/products/:id', getProductById);   
-// router.put('/products/:id', updateProduct);     
-// router.delete('/products/:id', deleteProduct);
+router.get('/products/:id', getProductById);
+router.put('/product/:id', updateProduct);
+router.delete('/product/:id', deleteProduct);
 
 export default router;
